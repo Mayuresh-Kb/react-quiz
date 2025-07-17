0a218d4603a8cacd2862c7aee3e4daa0d80256e5
Mayuresh-Kb <mayureshkasabe51@gmail.com>
2025-07-17 14:48:37 +0530

    fetched an api for questions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch(`http://localhost:8000/questions`)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error("ERROR!"));
+  });
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
