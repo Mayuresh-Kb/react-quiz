a1499deb4ae99053c826b4cd41a8c9ab56d25e17
Mayuresh-Kb <mayureshkasabe51@gmail.com>
2025-07-18 13:14:15 +0530

    feat: Loading questions from fake API

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ const initialState = {
 
 function reducer(state, action) {
   switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Action Unknown");
   }
 }
 
@@ -20,8 +26,8 @@ export default function App() {
   useEffect(function () {
     fetch(`http://localhost:8000/questions`)
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error("ERROR!"));
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   });
   return (
     <div className="app">
